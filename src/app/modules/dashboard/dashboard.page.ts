baf11f407092df17ab4ba6196e11f33c570a1400
Daniel Luque <yodaaniel123@gmail.com>
2023-10-02 16:48:13 -0400

    menu dashboard modificado

--- src/app/modules/dashboard/dashboard.page.ts
+++ src/app/modules/dashboard/dashboard.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem} from 'src/app/helpers/interfaces/menu.interface';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonMenu, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,46 +7,87 @@ import { MenuItem} from 'src/app/helpers/interfaces/menu.interface';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-  showOpciones = false;
-
-  toggleOpciones() {
-    this.showOpciones = !this.showOpciones;
-  }
-  menuItems:any[]=[
+  @ViewChild('menuid') menuclose!: IonMenu; // Importa IonMenu y ViewChild
+  
+  menu: any[] = [
     {
-      title:'inicio',
-      path:'/dashboard/inicio',
-      icon:'home-outline'
+      id: 1,
+      title: 'inicio',
+      multiple: false,
+      open: false,
+      path: '/dashboard/inicio',
+      icon: 'home-outline',
     },
     {
-      title:'Colegios',
-      path:'/dashboard/colegios',
-      icon:'shield-half-outline'
+      id: 2,
+      title: 'Colegios',
+      multiple: true,
+      open: false,
+      icon: 'shield-half-outline',
+      submenu: [
+        {
+          title: 'Listar colegios',
+          path: '/dashboard/colegios/listar-colegios',
+          icon: 'list-outline'
+        },
+        {
+          title: 'Crear nuevo colegio',
+          path: '/dashboard/colegios/crear-colegio',
+          icon: 'pencil-outline'
+        }
+      ],
     },
     {
-      title:'Proveedores',
-      path:'/dashboard/proveedores',
-      icon:'people-outline'
+      id: 3,
+      title: 'Proveedores',
+      multiple: true,
+      open: false,
+      icon: 'people-outline',
+      submenu: [
+        {
+          title: 'Listar proveedores',
+          path: '/dashboard/proveedores',
+          icon: 'list-outline'
+        },
+        {
+          title: 'Crear nuevo proveedor',
+          path: '/dashboard/proveedores/crear-proveedor',
+          icon: 'pencil-outline'
+        }
+      ],
     },
     {
-      title:'Productos',
-      path:'/dashboard/productos',
-      icon:'fast-food-outline'
+      id: 4,
+      title: 'Productos',
+      multiple: false,
+      open: false,
+      path: '/dashboard/productos',
+      icon: 'fast-food-outline'
     },
     {
-      title:'Informes',
-      path:'/dashboard/informes',
-      icon:'documents-outline'
+      id: 5,
+      title: 'Informes',
+      multiple: false,
+      open: false,
+      path: '/dashboard/informes',
+      icon: 'documents-outline'
     }
 
   ]
 
 
-  constructor() { }
+  constructor(public modalCtrl:ModalController) { }
 
   ngOnInit() {
   }
 
+  public mostrar(id: number) {
+    this.menu[id-1].open=!this.menu[id-1].open;
+  }
 
+
+  public cerrarmenu(){
+    this.menuclose.close(); // Cierra el menú
+  }
 
 }
